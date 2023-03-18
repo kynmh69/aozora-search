291c5559826e86e63148f05cd403bba9a6df1041
kynmh69 <hiroki.kayanuma@gmail.com>
2023-03-18 18:31:42 +0900

    mod main

--- cmd/aozora-search/main.go
+++ cmd/aozora-search/main.go
@@ -63,14 +63,31 @@ func main() {
 }
 
 func queryContent(db *sql.DB, s string) error {
-	rows, err := db.Query(s)
+	query := `
+	SELECT
+		a.author,
+		c.title_id,
+		c.title
+	FROM
+		contents c
+	INNER JOIN authors a
+		ON a.author_id = c.author_id
+	INNER JOIN contents_fts f
+		ON c.rowid = f.docid
+		AND words MATCH ?
+`
+	rows, err := db.Query(query, s)
 	if err != nil {
 		return err
 	}
+
+	columns, _ := rows.Columns()
+	log.Println(columns)
+
 	for rows.Next() {
-		var author_id, title_id, title, content string
-		rows.Scan(&author_id, &title_id, &title, &content)
-		log.Println(author_id, title_id, title, content)
+		var author, title_id, title string
+		rows.Scan(&author, &title_id, &title)
+		log.Println(author, title_id, title)
 	}
 	return nil
 }
@@ -90,15 +107,25 @@ func showContent(db *sql.DB, s1, s2 string) error {
 }
 
 func showTitles(db *sql.DB, s string) error {
-	query := "select (author_id, title_id, title, content) from contents where author_id = ?"
+	// query := "select (author_id, title, title_id, content) from contents"
+	query := "select * from contents where author_id = ?"
+	log.Printf("%s, %T", s, s)
 	rows, err := db.Query(query, s)
 	if err != nil {
 		return err
 	}
+
+	columns, err := rows.Columns()
+	if err != nil {
+		return err
+	}
+
+	log.Println(columns)
+
 	for rows.Next() {
 		var author_id, title_id, title, content string
-		rows.Scan(&author_id, &title_id, &title, &content)
-		log.Println(author_id, title_id, title, content)
+		rows.Scan(&author_id, &title, &title_id, &content)
+		log.Println(title_id, title)
 	}
 	return nil
 }
