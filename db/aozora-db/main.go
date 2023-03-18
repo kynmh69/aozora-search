24bc1045783a5c4d839b5c1ece6daacd8ab54a92
kynmh69 <hiroki.kayanuma@gmail.com>
2023-03-18 13:06:21 +0900

    mod db transaction.

--- db/aozora-db/main.go
+++ db/aozora-db/main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "database.sqlite")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer db.Close()
+
+	queries := []string{
+		`create table if not exists authors(author_id TEXT, auther TEXT, PRIMARY KEY(auther_id))`,
+		`create table if not exists contents(author_id TEXT, title_id TEXT, title TEXT, content TEXT PRIMARY KEY(autnor_id, title_id))`,
+		`create virtual table if not exists contents_fts USING fts4(words)`,
+	}
+	for _, q := range queries {
+		_, err = db.Exec(q)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+}
